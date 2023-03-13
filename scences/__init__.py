06594fad32b302998acfe1703c7e064a550b9c68
sssjf <18810969390@yeah.net>
2023-03-13 23:24:17 +0800

    Add Reverse Algorithm

--- scences/__init__.py
+++ scences/__init__.py
@@ -7,6 +7,7 @@ from scences.self_cars.self_car_velocity.lowspeed import LowSpeed
 from scences.self_cars.self_car_relation.cutin import Cutin
 from scences.self_cars.self_car_relation.cutout import Cutout
 from scences.self_cars.self_car_direction.go_straight import GoStraight
+from scences.self_cars.self_car_direction.reverse import Reverse
 from scences.others.others_relation.other_overtake import OtherOverTake
 from scences.others.others_velocity.other_acceleration import OtherAcceleration
 from scences.others.others_velocity.other_emerge_braking import OtherEmergeBraking
@@ -17,7 +18,7 @@ K_LOG = logging
 trans_coefficient = -1 * math.pi / 2700
 
 analyzers = [LowSpeed(), Cutin(), Cutout(), GoStraight(), OtherOverTake(), OtherAcceleration(), OtherEmergeBraking(),
-             Classification()]
+             Classification(), Reverse()]
 
 class_map = {
     "Car": 1,
